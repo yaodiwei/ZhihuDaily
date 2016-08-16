94a289c0745b8ac28ec34626d939c791f94af140
yaodiwei <yaodiwei2013@gmail.com>
2016-08-17 02:11:32 +0800

    研究support.design包控件并且应用

--- app/src/main/java/com/yao/zhihudaily/ui/feed/NewDetailActivity.java
+++ app/src/main/java/com/yao/zhihudaily/ui/feed/NewDetailActivity.java
@@ -2,10 +2,15 @@ package com.yao.zhihudaily.ui.feed;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.gson.Gson;
@@ -34,6 +39,7 @@ public class NewDetailActivity extends Activity {
     private WebView webView;
     private TextView tvTitle, tvSource;
     private ImageView ivImage;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
 
 
     @Override
@@ -42,6 +48,39 @@ public class NewDetailActivity extends Activity {
         setContentView(R.layout.activity_new_detail);
 
         final Story story = (Story) getIntent().getSerializableExtra("story");
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbarLayout.setTitle("五个推荐者");
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+//        collapsingToolbarLayout.setCollapsedTitleTextColor(0xFF000000);
+//        collapsingToolbarLayout.setExpandedTitleColor(0x00FFFFFF);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.mipmap.back);//设置导航栏图标
+//        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.inflateMenu(R.menu.new_detail_menu);//设置右上角的填充菜单
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.itemShare:
+                        Toast.makeText(NewDetailActivity.this, "点击分享", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.itemComment:
+                        Toast.makeText(NewDetailActivity.this, "点击评论", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
 
         webView = (WebView) findViewById(R.id.webView);
         tvTitle = (TextView) findViewById(R.id.tvTitle);
@@ -89,8 +128,6 @@ public class NewDetailActivity extends Activity {
                         Glide.with(NewDetailActivity.this).load(newJson.getImage()).into(ivImage);
                     }
                 });
-
-
 
 
     }
