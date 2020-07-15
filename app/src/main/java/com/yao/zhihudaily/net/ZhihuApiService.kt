338349a292f61945f76e84b845208860232f5582
yaodiwei <yaodiwei2013@gmail.com>
2020-07-15 15:29:13 +0800

    增加一个preRelease版本，实现正式环境域名可以抓包

--- app/src/main/java/com/yao/zhihudaily/net/ZhihuApiService.kt
+++ app/src/main/java/com/yao/zhihudaily/net/ZhihuApiService.kt
@@ -1,5 +1,6 @@
 package com.yao.zhihudaily.net
 
+import com.yao.zhihudaily.BuildConfig
 import com.yao.zhihudaily.model.*
 import io.reactivex.Observable
 import okhttp3.OkHttpClient
@@ -11,7 +12,11 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Streaming
 import retrofit2.http.Url
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.*
 
 /**
  * @author Yao
@@ -19,15 +24,54 @@ import java.util.concurrent.TimeUnit
  */
 interface ZhihuApiService {
 
+    private class TrustAllManager : X509TrustManager {
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        override fun getAcceptedIssuers(): Array<X509Certificate> {
+            return emptyArray()
+        }
+    }
+
+    private class TrustAllHostnameVerifier : HostnameVerifier {
+        override fun verify(hostname: String, session: SSLSession): Boolean {
+            return true
+        }
+    }
+
     companion object Factory {
 
         val ZHIHU_BASE_URL = "https://news-at.zhihu.com/api/"
 
         val mZhihuApiService = create()
 
+        fun createSSLSocketFactory(): SSLSocketFactory? {
+            var sSLSocketFactory: SSLSocketFactory? = null
+            try {
+                val sc = SSLContext.getInstance("TLS")
+                sc.init(null, arrayOf<TrustManager>(TrustAllManager()),
+                        SecureRandom())
+                sSLSocketFactory = sc.socketFactory
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return sSLSocketFactory
+        }
+
         fun create(): ZhihuApiService {
 
-            val okHttpClient = OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS).build()
+            val builder = OkHttpClient.Builder()
+            builder.connectTimeout(10, TimeUnit.SECONDS)
+            if (BuildConfig.DEBUG) {
+                builder.sslSocketFactory(createSSLSocketFactory()!!, TrustAllManager())
+                builder.hostnameVerifier(TrustAllHostnameVerifier())
+            }
+            val okHttpClient = builder.build()
 
             val retrofit = Retrofit.Builder()
                     .client(okHttpClient)
